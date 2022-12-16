696dec8a1c5af702f934f624fe20c3b53a11f2c1
Martinus Lukas <martinus.lukas@gmail.com>
2022-12-16 17:49:53 +0800

    Aplikasi TodoList menggunakan H2 Database hehe

---
+++ simple-todolist/src/test/java/com/tugas/simpletodolist/SimpleTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugas.simpletodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
